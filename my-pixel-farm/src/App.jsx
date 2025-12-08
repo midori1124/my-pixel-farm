a1283108fa3910e829cc90154c2820f43e1b23ec
midori1124 <yequan78@gmail.com>
2025-12-08 12:29:02 +0900

    更新状态

--- my-pixel-farm/src/App.jsx
+++ my-pixel-farm/src/App.jsx
@@ -355,8 +355,8 @@ const App = () => {
                               <Code size={32} className="text-[#5E2C0C]" />
                           </div>
                           <div>
-                              <div className="font-bold text-[#5E2C0C]">状态: 高效工作中</div>
-                              <div className="text-lg text-[#8E4918]">咖啡因 Buff 生效中 (剩余 2h)</div>
+                              <div className="font-bold text-[#5E2C0C]">状态: 低效上学</div>
+                              <div className="text-lg text-[#8E4918]">咖啡因 Buff 生效中 (剩余 12h)</div>
                           </div>
                         </div>
                       </div>
